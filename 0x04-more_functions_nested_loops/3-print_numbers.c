14cc300a2282524fcd0397d495d884ed6fd4374d
tsucess <taofeeq.muhammad22@gmail.com>
2023-03-23 07:51:43 -0700

    úpdated

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - function that prints numbers 0 - 9
+ *
+ * Return: mul.
+ */
+void print_numbers(void)
+{
+	char num;
+
+	for (num = 0; num < 10; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
