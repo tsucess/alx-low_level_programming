8f531e5b8c11373ade855fdfe6103ef504bbdc22
tsucess <taofeeq.muhammad22@gmail.com>
2023-03-27 14:51:21 -0700

    task file uploading

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - fuction that takes pointer to an integer as parameter
+ * @n: the character
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
