6e4a0f6ad5991be0e62c66b9eab28acabad3baf4
tsucess <taofeeq.muhammad22@gmail.com>
2023-03-27 21:53:32 -0700

    updated

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -9,7 +9,7 @@ int _strlen(char *s)
 {
 	int len;
 
-	for (len = 0; len != '\0'; len++)
+	for (len = 0; *s != '\0'; len++)
 	{
 		s++;
 	}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: string character
+ */
+void print_rev(chare *s)
+{
+	int i = 0;
+
+	while(s[i] != '\0')
+	{
+		i++;
+		for (i = i; i>= 0; i--)
+		{
+			_putchar(s[i]);
+		}
+		_putchar('\n');
+	}
+}
