a06d8bf1ed75c8f6a54f73df926545818323fe57
tsucess <taofeeq.muhammad22@gmail.com>
2023-03-30 14:20:09 -0700

    updated

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet - function that econdes a string into  1337
+ * @n: strings to be replaced
+ * Return: the value of n
+ */
+char *leet(char *n)
+{
+	int a, b;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0; n[a] != '\0'; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			if (n[i] == s1[b])
+			{
+				n[a] = s2[b];
+			}
+		}
+	}
+	return (n);
+}
