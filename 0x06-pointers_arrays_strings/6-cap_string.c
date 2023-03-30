f9ca166f501df3df5d46877dd88ec85c914de5fb
tsucess <taofeeq.muhammad22@gmail.com>
2023-03-30 14:08:46 -0700

    updated

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes strings
+ * @str: string
+ * Return: pointer to string
+ */
+char *cap_string(char *str)
+{
+	int a = 0;
+
+	while (str[a])
+	{
+		while (!(str[a] >= 'a' && str[a] <= 'z'))
+		{
+			a++;
+			if (str[a - 1] == ' ' || str[a - 1] == '\t' ||
+				str[a - 1] == ',' || str[a - 1] == '\n' ||
+				str[a - 1] == ';' || str[a - 1] == '.' ||
+				str[a - 1] == '!' || str[a - 1] == '?' ||
+				str[a - 1] == '"' || str[a - 1] == '(' ||
+				str[a - 1] == ')' || str[a - 1] == '{' ||
+				str[a - 1] == '}' || a == 0)
+				str[a] -= 32;
+			a++;
+		}
+
+	}
+	return (str);
+}
