da7ed4bb69a35864d137e1db03b0929414d57505
tsucess <taofeeq.muhammad22@gmail.com>
2023-04-04 15:46:15 -0700

    task 7 updated

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,12 +5,12 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int c = 0;
-	int b = 0;
+	int c, b;
 
-	for (; c < 8; c++)
+	for (c = 0; c < 8; c++)
 	{
-		for (; b < 8; b++)
+
+		for (b = 0; b < 8; b++)
 			_putchar(a[c][b]);
 		_putchar('\n');
 	}
