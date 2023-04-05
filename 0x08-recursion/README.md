6d02396b89f6591c382de34b10cd3810dbf6162a
tsucess <taofeeq.muhammad22@gmail.com>
2023-04-05 14:03:47 -0700

    task 0 print string recursively

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+ALX tasks on Recursion task
---
+++ 0x08-recursion/test.main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "_putchar.c"
+#include "0-puts_recursion.c"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    _puts_recursion("Puts with recursion");
+    return (0);
+}
