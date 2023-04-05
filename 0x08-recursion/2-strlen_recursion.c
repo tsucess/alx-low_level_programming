a384bdf2194af9e4a15343dff57d6db19c03d98a
tsucess <taofeeq.muhammad22@gmail.com>
2023-04-05 14:44:09 -0700

    print string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - func that print length of string
+ * @s: string parameter
+ * Return: strings length
+ */
+int _strlen_recursion(char *s)
+{
+	int a = 0;
+
+	if (*s != '\0')
+	{
+		a += _strlen_recursion(s + 1);
+
+	}
+	return (a);
+
+}
