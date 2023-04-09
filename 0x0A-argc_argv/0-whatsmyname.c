188bdc7a9929a6c9a2178502475a264fe0ac43d0
tsucess <taofeeq.muhammad22@gmail.com>
2023-04-09 16:15:31 -0700

    betty error cleaned

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,13 +2,13 @@
 /**
  * main - Entry point
  * @argc: counter parameter
- * @arvv: array parameter
+ * @argv: array parameter
  *
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	printf ("%s\n", argv[argc]);
+	printf("%s\n", argv[argc]);
 
 	return  (0);
 }
