34ce75db45ef77c16f702e5dc98406dd80f72cdb
tsucess <taofeeq.muhammad22@gmail.com>
2023-04-10 05:08:58 -0700

    task 1 program that print number of argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: counter parameter
+ * @argv: array parameter
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%d\n", argc);
+
+	return  (0);
+}
