c7a30d56c177f9b731bcb736a70d8c5fff7e8066
tsucess <taofeeq.muhammad22@gmail.com>
2023-04-09 23:46:37 -0700

    task 0 print my name(filename)

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - Entry point
  * @argc: counter parameter
@@ -8,7 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[argc]);
+	printf("%s\n", argv[0]);
 
 	return  (0);
 }
