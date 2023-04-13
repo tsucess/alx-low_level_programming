c4005e50d129659de3e53fa13a4ae48fd13ac01a
tsucess <taofeeq.muhammad22@gmail.com>
2023-04-13 15:38:50 -0700

    task 3, function that creates an array in a specified range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -8,9 +8,16 @@
  */
 int *array_range(int min, int max)
 {
-	int *arr, a = 0;
+	int *arr, a = 0, size;
 
 	if (min > max)
+	{
+		return (NULL);
+	}
+	size = max - min + 1;
+	arr = malloc(sizeof(int) * size);
+
+	if (arr == NULL)
 	{
 		return (NULL);
 	}
