f3d2a7967f69bf70fb2b7b40109a828b0c906fa6
tsucess <taofeeq.muhammad22@gmail.com>
2023-04-16 05:17:41 -0700

    error fixed

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,26 +20,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s2[len2] != '\0')
 		len2++;
 
-	if (n < len2)
+	if (n >= len2)
 	{
-		news = malloc(sizeof(char) * (len1 + n + 1));
+		n = len2;
 	}
-	else
-	{
-		news = malloc(sizeof(char) * (len1 + len2 + 1));
-	}
-	if (!news)
+	news = malloc(sizeof(char) * (len1 + n + 1));
+
+	if (news == NULL)
 		return (NULL);
-	while (a < len1)
+
+	while (s1[len1] == '\0')
 	{
 		news[a] = s1[a];
 		a++;
 	}
-	while (n < len2 && a < (len1 + n))
-	{
-		news[a++] = s2[b++];
-	}
-	while (n >= len2 && a < (len1 + len2))
+	while (b < n && s2[len2] == '\0')
 	{
 		news[a++] = s2[b++];
 	}
