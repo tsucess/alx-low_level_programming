f25c424667e2635d205baed2f8aa0ab02e55b9f5
tsucess <taofeeq.muhammad22@gmail.com>
2023-04-17 10:42:23 -0700

    directory and task 0 dog.h uploaded

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+ALX - task 0x0E on structures and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct dog declaration
+ * @name: first dog element with char data type
+ * @age: second dog element with float data type
+ * @owner: third dog element with char data type
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
