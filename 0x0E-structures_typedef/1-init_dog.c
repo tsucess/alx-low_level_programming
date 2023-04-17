dcf2acfd42b6ad9e86687ec7a3295360eb0ab7bf
tsucess <taofeeq.muhammad22@gmail.com>
2023-04-17 14:02:47 -0700

    task 1 uploaded function that initialize struct element

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include "dog.h"
 /**
  * init_dog - function that initializes struct dog
  * @d: struct parameter
@@ -11,8 +12,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	d = malloc(sizeof(struct dog));
+
 	if (d == NULL)
-		d = malloc(sizeof(struct dog));
+		return (NULL);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
