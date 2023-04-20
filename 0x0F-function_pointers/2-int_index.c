e8949fa139a37181d98466abf46741db8b201eb4
tsucess <taofeeq.muhammad22@gmail.com>
2023-04-19 20:51:52 -0700

    task 2 search for integer with function

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+/**
+ * int_index - function that searches for an integer
+ * in an array
+ * @array: array parameter
+ * @size: size of the array to search from
+ * @cmp: compare function
+ * Return: the -1 or 0
+ */
+int int_index(int *array, int size, int (*cmp)(int));
+{
+	int a;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+	{
+		return (-1);
+	}
+
+	while (a < size)
+	{
+		if (cmp(array[a]))
+		{
+			return (a);
+		}
+		a++;
+	}
+	return (-1);
+}
