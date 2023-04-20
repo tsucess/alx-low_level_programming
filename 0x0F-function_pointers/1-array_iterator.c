a379b9a9da8c9c903b56311c50c1fa971afc03e7
tsucess <taofeeq.muhammad22@gmail.com>
2023-04-19 20:38:00 -0700

    tak 1 fuction that executes a function as parameter

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "fucntion_pointers.h"
+/**
+ * array_iterator - function that takes a function as parameter
+ * @array: array parameter
+ * @size: size of rhe array
+ * @action: function parameter
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int a = 0;
+
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+	while (a < size)
+	{
+		action(array[i]);
+		a++;
+	}
+}
