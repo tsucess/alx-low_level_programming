92843592fee0849c9374276cdff66eca9e2b76e0
tsucess <taofeeq.muhammad22@gmail.com>
2023-04-19 21:23:05 -0700

    error fixed

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,29 +1,27 @@
 #include "function_pointers.h"
-#include <stdio.h>
 /**
  * int_index - function that searches for an integer
  * in an array
  * @array: array parameter
  * @size: size of the array to search from
  * @cmp: compare function
- * Return: the -1 or 0
+ * Return: 0
  */
 int int_index(int *array, int size, int (*cmp)(int));
 {
-	int a = 0;
+	int a;
 
 	if (array == NULL || size <= 0 || cmp == NULL)
 	{
 		return (-1);
 	}
 
-	while (a < size)
+	for (a = 0; a < size; a++)
 	{
 		if (cmp(array[a]))
 		{
 			return (a);
-		}
-		a++;
+		}	
 	}
 	return (-1);
 }
