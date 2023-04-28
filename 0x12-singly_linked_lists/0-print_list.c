7671951f5fc788d60adbc35cd7e70336701856c6
tsucess <taofeeq.muhammad22@gmail.com>
2023-04-28 13:56:26 -0700

    task 0 re-checked and fixed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * print_list - prints all the elements of a list_t list.
+ * print_list - function that prints all the elements of a list_t list.
+ *
  * @h: singly linked list.
  * Return: number of elements in the list.
  */
