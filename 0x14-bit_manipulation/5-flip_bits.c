9d644a3d9e178fff56d902bf483be2cc22fad3d1
tsucess <taofeeq.muhammad22@gmail.com>
2023-05-04 17:47:27 -0700

    task 5 bitwise error fixed

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -16,7 +16,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 	for (a = 63; a >= 0; a--)
 	{
-		curr = exclusive >> i;
+		curr = exclusive >> a;
 		if (curr & 1)
 			count++;
 	}
