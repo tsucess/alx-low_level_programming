0ed8890e89ea4268d29df103b44cbfd0bd9ebb12
tsucess <taofeeq.muhammad22@gmail.com>
2023-05-04 17:41:15 -0700

    task 5 loop error fixed

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - function that counts the number of bits to change
+ * to get from one number to another
+ * @n: first number parameter
+ * @m: second number parameter
+ *
+ * Return: the number of bits to change
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int a, count = 0;
+	unsigned long int curr;
+	unsigned long int exclusive = n ^ m;
+
+	for (a = 63; a >= 0; a--)
+	{
+		curr = exclusive >> i;
+		if (curr & 1)
+			count++;
+	}
+
+	return (count);
+}
